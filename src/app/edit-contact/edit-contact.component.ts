cc62850de7e5e1318e4688974775269626ea59cb
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-02-06 18:22:07 +0200

    first commit

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
+import { ActivatedRoute } from '@angular/router'
+import { ContactsService } from '../contacts/contacts.service'
 
 @Component({
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  contactForm = new FormGroup({
+  firstName : new FormControl(),
+  lastName : new FormControl(),
+  dateOfBirth : new FormControl(),
+  favoritesRanking : new FormControl()
+})
+
+
+  constructor(private route: ActivatedRoute
+   , private contactsService : ContactsService) { }
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
     if (!contactId) return
+
+    this.contactsService.getContact(contactId).subscribe((contact) => {
+      if (!contact) return;
+
+      this.contactForm.controls.firstName.setValue(contact.firstName)
+    })
   }
 
   saveContact() {
-
+     console.log(this.contactForm.value);
   }
 }
